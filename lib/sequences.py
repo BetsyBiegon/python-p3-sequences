0dd562232918d1c2c0b4ee544ed7124663685003
BetsyBiegon <betsybiegon@gmail.com>
2024-05-22 17:26:04 +0300

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+
+    fibonacci = [0]
+    if length > 1:
+        fibonacci.append(1)
+        for i in range(2, length):
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+    
+    print(fibonacci)
